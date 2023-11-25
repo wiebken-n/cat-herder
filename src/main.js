d06d286fcd78bdb7f8bbc86e27a9a1bb0f49508c
wiebken-n <126778886+wiebken-n@users.noreply.github.com>
2023-11-25 14:33:38 +0100

    Cleanup

--- src/main.js
+++ src/main.js
@@ -1,24 +1,35 @@
+/* eslint-disable vue/no-reserved-component-names */
+/* eslint-disable vue/multi-word-component-names */
+
 import './assets/base-style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+
 import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/lara-light-blue/theme.css'
+
+/* Prime Vue Component imports >>>>>>>> */
 
 // import Slider from 'primevue/slider'
 // import Knob from 'primevue/knob'
 import Button from 'primevue/button'
 
-import App from './App.vue'
-import router from './router'
+/* <<<<<<<<<<<< Prime Vue Component imports */
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue)
 
-/* Prime Vue Components */
+/* Prime Vue Components >>>>>>>> */
+
 // app.component('Slider', Slider)
 // app.component('Knob', Knob)
 app.component('Button', Button)
+
+/* <<<<<<< Prime Vue Components */
+
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
-// import CatDataView from '@/views/CatDataView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,14 +17,6 @@ const router = createRouter({
       path: '/cats',
       name: 'cats',
       component: () => import('../views/CatsView.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     },
     {
       path: '/catdata/:id',
